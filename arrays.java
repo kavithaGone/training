e1c40d1d2c87b5559dc21ab59939d49c73feec53
kavithaGone <kgone@rythmos.com>
2018-01-09 10:40:39 +0530

    initial changes

---
+++ arrays.java
@@ -0,0 +1,18 @@
+package com.kavitha.examples2;
+
+public class arrays
+{
+public static void main(String []args)
+{
+	int a1[]=new int[5];
+	a1[0]=23;
+	a1[1]=32;
+	a1[2]=21;
+	a1[3]=45;
+	a1[4]=11;
+	for(int i=0;i<5;i++) 
+	{
+		System.out.println(a1[i]);
+	}
+}	
+}
